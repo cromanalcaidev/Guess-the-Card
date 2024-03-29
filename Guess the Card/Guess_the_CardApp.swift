ca3ab2ca5c759d6f812480e3fef686ee9dd33326
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-03-29 17:58:08 +0100

    Initial Commit

---
+++ Guess the Card/Guess_the_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_CardApp.swift
+//  Guess the Card
+//
+//  Created by Carlos Román Alcaide on 29/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
